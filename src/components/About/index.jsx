2cedee5a6ad743253370f474d277305e2f528380
SimranKaur2628 <65796728+SimranKaur2628@users.noreply.github.com>
2020-11-30 23:39:21 -0500

    .

---
+++ src/components/About/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../shared/Header';
+
+const Home = () => {
+  return (
+    <> 
+      {(Header)/* You need to use your shared header component... */}
+      {paragraph/* You need a paragraph describing a fun fact about you */}
+      
+    </>
+  );
+}
+ 
+export default Home;
---
+++ src/components/shared/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './styles.css';
+
+const Header = ({title}) => {
+  return (
+    <style.Header>
+      <h1>{title = "Simran"}React heroku Lab-2</h1>
+    </style.Header>
+  );
+}
+ 
+export default Header;
